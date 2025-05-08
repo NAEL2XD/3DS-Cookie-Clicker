5bb0cd40579cd66510c7af353a62f78a40254d78
NAEL2XD <108278470+NAEL2XD@users.noreply.github.com>
2025-05-08 18:21:47 +0200

    hide cookie

--- source/game.c
+++ source/game.c
@@ -28,8 +28,7 @@ int game_init() {
     game.textSize = 0;
     game.cookiePerPress = 1;
 
-    printf("PLEASE WORK");
-    game.sprites.cookie = UTILS_loadImage("romfs:/assets/cookie.t3x");
+    // game.sprites.cookie = UTILS_loadImage("romfs:/assets/cookie.t3x");
     return 0;
 }
 
@@ -63,7 +62,7 @@ bool game_update() {
 
     float finalSize = -(game.textSize / 6);
     UTILS_renderBorderText(cookieShit, -1, -10 + (20 / (game.textSize + 1)), 1, game.textSize + 1);
-    C2D_DrawImageAtRotated(game.sprites.cookie, 200 + (finalSize * 6), 128 + (finalSize * 6), 0, sin((osGetTime() - curTime) / 800) / 8, NULL, finalSize + 1.2, finalSize + 1.2);
+    // C2D_DrawImageAtRotated(game.sprites.cookie, 200 + (finalSize * 6), 128 + (finalSize * 6), 0, sin((osGetTime() - curTime) / 800) / 8, NULL, finalSize + 1.2, finalSize + 1.2);
     game.textSize /= 1.1;
 
     for (int i = 0; i < stCount; i++) {
